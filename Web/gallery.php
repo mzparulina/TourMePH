fbd8fad7cbbf1ace651be4fc758a781fe42d78a1
Pauline Revilla <p_revilla@ymail.com>
2014-07-27 10:01:18 +0800

    Update gallery.php

--- Web/gallery.php
+++ Web/gallery.php
@@ -258,7 +258,7 @@
     <script src="js/plugins/morris/raphael.min.js"></script>
     <script src="js/plugins/morris/morris.min.js"></script>
     <script src="js/plugins/morris/morris-data.js"></script>
-
+ 
 </body>
 
 </html>
